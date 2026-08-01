b45abe0cb05610efd772a016d1e074d24f3c915b
sivakavin4545-sys <sivakavin4545@gmail.com>
2026-08-01 19:16:09 +0530

    Time: 17 ms (27.72%) | Memory: 12.6 MB (19.51%) - LeetSync

---
+++ 6-zigzag-conversion/zigzag-conversion.py
@@ -0,0 +1,19 @@
+class Solution:
+    def convert(self, s, numRows):
+        if numRows == 1 or numRows >= len(s):
+            return s
+
+        rows = [""] * numRows
+        row, step = 0, 1
+
+        for char in s:
+            rows[row] += char
+
+            if row == 0:
+                step = 1
+            elif row == numRows - 1:
+                step = -1
+
+            row += step
+
+        return "".join(rows)
