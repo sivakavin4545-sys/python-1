c4f16ade5561fe3106320ec4833099fb9c281013
sivakavin4545-sys <sivakavin4545@gmail.com>
2026-08-20 15:12:24 +0530

    Time: 0 ms (100.00%) | Memory: 12.2 MB (91.63%) - LeetSync

---
+++ 94-binary-tree-inorder-traversal/binary-tree-inorder-traversal.py
@@ -0,0 +1,8 @@
+class Solution:
+    def inorderTraversal(self, root):
+        if not root:
+            return []
+        
+        return (self.inorderTraversal(root.left) +
+                [root.val] +
+                self.inorderTraversal(root.right))
