4a8614cd3cc77b11e6514e55542e5440508d39f0
sivakavin4545-sys <sivakavin4545@gmail.com>
2026-08-20 16:08:50 +0530

    Time: 4 ms (93.51%) | Memory: 12.7 MB (24.28%) - LeetSync

--- 10-regular-expression-matching/regular-expression-matching.py
+++ 10-regular-expression-matching/regular-expression-matching.py
@@ -2,21 +2,31 @@ class Solution:
     def isMatch(self, s, p):
         memo = {}
 
-        def match(i, j):
+        def dp(i, j):
+            # Have we already solved this state?
             if (i, j) in memo:
                 return memo[(i, j)]
 
+            # Pattern is finished: string must also be finished
             if j == len(p):
                 return i == len(s)
 
-            first_match = i < len(s) and (p[j] == s[i] or p[j] == ".")
+            # Does current pattern character match current string character?
+            first_match = (
+                i < len(s) and
+                (p[j] == s[i] or p[j] == ".")
+            )
 
+            # Next pattern character is '*'
             if j + 1 < len(p) and p[j + 1] == "*":
-                result = match(i, j + 2) or (first_match and match(i + 1, j))
+                result = (
+                    dp(i, j + 2) or              # Use zero occurrences
+                    (first_match and dp(i + 1, j)) # Use one or more
+                )
             else:
-                result = first_match and match(i + 1, j + 1)
+                result = first_match and dp(i + 1, j + 1)
 
             memo[(i, j)] = result
             return result
 
-        return match(0, 0)
+        return dp(0, 0)
