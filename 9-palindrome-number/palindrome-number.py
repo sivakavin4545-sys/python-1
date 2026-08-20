889c712724bff8b2d81939fca552eaa2bed5d488
sivakavin4545-sys <sivakavin4545@gmail.com>
2026-08-20 16:07:51 +0530

    Time: 8 ms (71.73%) | Memory: 12.2 MB (90.45%) - LeetSync

--- 9-palindrome-number/palindrome-number.py
+++ 9-palindrome-number/palindrome-number.py
@@ -1,6 +1,17 @@
 class Solution:
     def isPalindrome(self, x):
-        if x < 0:
+        # Negative numbers and numbers ending in 0 are not palindromes,
+        # except 0 itself.
+        if x < 0 or (x % 10 == 0 and x != 0):
             return False
 
-        return str(x) == str(x)[::-1]
+        reversed_half = 0
+
+        # Reverse only the second half of the number
+        while x > reversed_half:
+            reversed_half = reversed_half * 10 + x % 10
+            x //= 10
+
+        # Even digits: x == reversed_half
+        # Odd digits: middle digit can be ignored
+        return x == reversed_half or x == reversed_half // 10
